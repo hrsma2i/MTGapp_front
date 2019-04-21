bb48519acc4ac4df9f35efaaba3ffc59418751c4
hrsma2i <hrs.ma2i@gmail.com>
2019-04-22 08:52:02 +0900

    [WIP] Register.vue の作成

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,13 +1,43 @@
 <template>
-  <v-container>
+  <!-- <v-container fill-height> -->
     <v-layout
+      fill-height
       text-xs-center
       wrap
+      justify-center
+      column
+      class="mx-3"
     >
-    register
-    test
+      <!-- <v-spacer></v-spacer> -->
+      <v-layout align-end>
+        <v-flex
+            display-1
+            font-weight-medium
+            text-xs-center
+        >
+          Time is<br>
+          M💰ney Pacquio!
+        </v-flex>
+      </v-layout>
+      <v-layout align-center>
+        <v-flex>
+          <div>
+            <v-text-field
+              color="light-blue"
+              label="メールアドレス"></v-text-field>
+          </div>
+        </v-flex>
+      </v-layout>
+      <v-flex>
+        <div>
+          <v-btn color="light-blue">
+          <!-- <v-btn color=rgba(69,142,202,255)> -->
+            ログイン
+          </v-btn>
+        </div>
+      </v-flex>
     </v-layout>
-  </v-container>
+  <!-- </v-container> -->
 </template>
 
 <script>
